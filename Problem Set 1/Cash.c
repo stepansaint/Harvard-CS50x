8cddcb34f5b202b2d8f92bae1298960394970592
Ermakov Stepan <110097131+stepansaint@users.noreply.github.com>
2022-11-28 13:27:51 +0300

    Reimplemented PSet1

--- Problem Set 1/Cash.c
+++ Problem Set 1/Cash.c
@@ -1,35 +1,35 @@
 /**
- * Implemented without cs50.h
- * @author Stepan Ermakov
+ * Implemented without CS50 library
+ * @author Ermakov Stepan
 */
 
 #include <stdio.h>
 
+int calculate_dimes(int change);
+int calculate_nickels(int change);
+int calculate_pennies(int change);
+int calculate_quarters(int change);
+int get_cents();
+int get_int(char *strig);
+
 // A program that prompts the user for the number of cents 
 // that a customer is owed and then prints the smallest number 
-// of coins with which that change can be made.
-
-int get_cents()
+// of coins with which that change can be made
+int main(void)
 {
-    int change;
-    do
-    {
-        change = get_int("How much change is owed?\n");
-    }
-    while (change < 0);
-    return change;
-}
+    int change = get_cents();
 
-int calculate_quarters(int change)
-{
-    int numberOfCoins = 0;
-    int quarter = 25;
-    while (change >= quarter)
-    {
-        numberOfCoins++;
-        change -= quarter;
-    }
-    return numberOfCoins;
+    int numberOfQuarters = calculate_quarters(change);
+    change -= numberOfQuarters * 25;
+    int numberOfDimes = calculate_dimes(change);
+    change -= numberOfDimes * 10;
+    int numberOfNickels = calculate_nickels(change);
+    change -= numberOfNickels * 5;
+    int numberOfPennies = calculate_pennies(change);
+
+    int numberOfCoins = numberOfQuarters + numberOfDimes +
+                        numberOfNickels + numberOfPennies;
+    printf("%i\n", numberOfCoins);
 }
 
 int calculate_dimes(int change)
@@ -68,18 +68,35 @@ int calculate_pennies(int change)
     return numberOfCoins;
 }
 
-int main(void)
+int calculate_quarters(int change)
 {
-    int change = get_cents();
-    int numberOfQuarters = calculate_quarters(change);
-    change -= numberOfQuarters * 25;
-    int numberOfDimes = calculate_dimes(change);
-    change -= numberOfDimes * 10;
-    int numberOfNickels = calculate_nickels(change);
-    change -= numberOfNickels * 5;
-    int numberOfPennies = calculate_pennies(change);
-    int numberOfCoins = numberOfQuarters + numberOfDimes +
-                        numberOfNickels + numberOfPennies;
-
-    printf("%i\n", numberOfCoins);
+    int numberOfCoins = 0;
+    int quarter = 25;
+    while (change >= quarter)
+    {
+        numberOfCoins++;
+        change -= quarter;
+    }
+    return numberOfCoins;
 }
+
+int get_cents()
+{
+    int change;
+    do
+    {
+        change = get_int("Change owed (in cents): ");
+    }
+    while (change <= 0);
+    return change;
+}
+
+int get_int(char *question)
+{
+    int n;
+    printf("%s", question);
+    scanf("%i", &n);
+    return n;
+}
+
+
---
+++ Problem Set 1/README.md
@@ -0,0 +1,3 @@
+## Usage
+To compile: make Cash.c (clang -o Cash Cash.c)
+To run: ./Cash
